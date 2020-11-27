b6c5047192061059977d663e105af77e70b440ce
IvanBazan <i-baz.1@ya.ru>
2020-11-28 00:57:39 +0300

    Try to connect API with table

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from typing import List
 import pandas as pd
 
 dataTable = pd.read_table('signatures.tsv').rename(columns={'Unnamed: 0': 'id'})
-# pd.read_csv('signatures.tsv', sep='\t', index_col='Unnamed: 0')
+indexTable = dataTable.id
+
+
+dataTable = pd.read_csv('signatures.tsv', sep='\t', index_col='Unnamed: 0')
 # dataTable.loc[dataTable.id == 'NSCLC1352']
 
 app = FastAPI()
@@ -19,5 +22,5 @@ def get_book(q: List[str] = Query(..., description="Search book")):
 
 @app.get('/getIDs')
 def get_ids():
-    return dataTable.loc[dataTable.id == 'NSCLC1352']
+    return dataTable.loc[dataTable.id == 'NSCLC1352'].T
 
